be892ea8d289da503b4af48046cfb48c075c2587
ryanyue123 <yue.ryan123@gmail.com>
2016-06-29 09:45:11 +0800

    Created Xcode project

---
+++ Annieparts/Annieparts/CatalogTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CatalogTableViewController.swift
+//  Annieparts
+//
+//  Created by Ryan Yue on 6/29/16.
+//  Copyright © 2016 boyang. All rights reserved.
+//
+
+import UIKit
+
+class CatalogTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCellWithIdentifier("product_cell") as! ProductTableViewCell
+        return cell
+    }
+}
---
+++ Annieparts/Annieparts/ProductTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProductTableViewCell.swift
+//  Annieparts
+//
+//  Created by Ryan Yue on 6/29/16.
+//  Copyright © 2016 boyang. All rights reserved.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
